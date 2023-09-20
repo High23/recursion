93bf4bc352de1d5fe90d6f6c6cce5745d8c47807
High23 <warenasb2@gmail.com>
2023-09-19 20:11:01 -0700

    Added merge sort function

--- index.js
+++ index.js
@@ -10,5 +10,32 @@ function fibsRec(seqNum) {
     }
 }
 
-fibsRec(8)
 
+let arr = [ 4, 8, 6, 2,
+            1, 7, 5, 3,
+            9
+          ]
+
+function mergeSort(givenArray) {
+    if (givenArray.length < 2) return givenArray;
+    else {
+        let leftHalf = mergeSort(givenArray.splice(0, Math.floor(givenArray.length / 2)));
+        let rightHalf = mergeSort(givenArray)
+        let j = 0;
+        for (let i = 0; i < leftHalf.length; i++) {
+            if (rightHalf[j] > leftHalf[i]) {
+                leftHalf.splice(i + 1, 0, rightHalf[j])
+                i = leftHalf.indexOf(rightHalf[j], i + 1)
+            } else if (rightHalf[j] < leftHalf[i]){
+                leftHalf.splice(i, 0, rightHalf[j])
+                i = leftHalf.indexOf(rightHalf[j], i)
+            } else {
+                break;
+            }
+            j = j + 1
+        }
+        return leftHalf
+    }
+}
+
+console.log((mergeSort(arr)))
